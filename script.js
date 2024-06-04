722e497222879922de398d2fd45e193fa5f487c9
N3nT <41384370+N3nT@users.noreply.github.com>
2024-06-04 13:04:22 +0200

    Fixes

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ const form_wrap = document.querySelector('.wrap')
 const succes_email = document.querySelector('.success_email')
 
 const validateEmail = () => {
-    email = email_input.value
-    if(!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-        error_label.style.display = 'block'
-        email_input.classList.add('input_error')
+    if (email_input.value != ''){
+        email = email_input.value
+        if(!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+            error_label.style.display = 'block'
+            email_input.classList.add('input_error')
     } else {
-        error_label.style.display = 'none'
-        email_input.classList.remove('input_error')
-        succes_wrap.style.display = 'block'
-        form_wrap.style.display = 'none'
-        email_input.value = ''
-        change_email(email)
+            error_label.style.display = 'none'
+            email_input.classList.remove('input_error')
+            succes_wrap.style.display = 'block'
+            form_wrap.style.display = 'none'
+            email_input.value = ''
+            change_email(email)
+        }
     }
   }
 
